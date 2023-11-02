11d86bafb1621e81b411c754eb084b0109415e4a
Maia <maiaxmarin@gmail.com>
2023-11-02 14:14:17 +0200

    Setup commit

---
+++ src/main/java/com/example/gdscjavaspring/GdscJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.gdscjavaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdscJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdscJavaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gdscjavaspring/GdscJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gdscjavaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdscJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
